5b3b0ce468602a81261a4e82d209d710f0773f77
ThePusher <spunkspunkik@gmail.com>
2022-02-22 10:32:15 +0200

    sort working

--- soccer_ranking.rb
+++ soccer_ranking.rb
@@ -38,7 +38,7 @@ class SoccerRanking
   end
 
   def sort_results
-    self.result_score = self.result_score.sort_by {|key, value| [value,key]}.reverse.to_h
+    self.result_score = self.result_score.sort_by {|key, value| [-value,-key]}.to_h
   end
 
   def print_result
